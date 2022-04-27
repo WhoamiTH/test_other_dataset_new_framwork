63c8f9d611fabe19be6773970c5cd8478286b7e5
taihan <taihan@bytedance.com>
2022-04-28 05:06:07 +0800

    test torch

--- test_mlp/mlp_change_glass0_train&test/test.py
+++ test_mlp/mlp_change_glass0_train&test/test.py
@@ -6,9 +6,27 @@ import predict_test
 import numpy as np
 import csv
 import re
+import handle_data
+import predict_test
 import pandas as pd
-import tensorflow as tf
-import sklearn.metrics as skmet
+import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_curve, auc, f1_score
+# import tensorflow as tf
+
+# import tensorflow.compat.v1 as tf
+# tf.disable_v2_behavior()
+
+
+import torch
+from torch import nn
+from torch.nn import init
+
+
+from sklearn.metrics import confusion_matrix
 
 
 import os
