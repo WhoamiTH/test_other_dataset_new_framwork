90ab816d2e12617b012c703938a1a30de0649c4c
taihan <taihan@bytedance.com>
2022-04-28 10:04:14 +0800

    server_test

--- test_mlp/mlp_change_glass0_train&test/execute_correct.sh
+++ test_mlp/mlp_change_glass0_train&test/execute_correct.sh
@@ -1,5 +1,5 @@
-nphup bash execute_correct_1.sh > ec1.log 2>&1 &
-nphup bash execute_correct_2.sh > ec2.log 2>&1 &
-nphup bash execute_correct_3.sh > ec3.log 2>&1 &
-nphup bash execute_correct_4.sh > ec4.log 2>&1 &
-nphup bash execute_correct_5.sh > ec5.log 2>&1 &
+nohup bash execute_correct_1.sh > ec1.log 2>&1 &
+nohup bash execute_correct_2.sh > ec2.log 2>&1 &
+nohup bash execute_correct_3.sh > ec3.log 2>&1 &
+nohup bash execute_correct_4.sh > ec4.log 2>&1 &
+nohup bash execute_correct_5.sh > ec5.log 2>&1 &
