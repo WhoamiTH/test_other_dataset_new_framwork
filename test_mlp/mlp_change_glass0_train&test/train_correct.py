d45423b1091f7f10035f40bc3574c29fa064a8e6
taihan <taihan@bytedance.com>
2022-04-28 09:52:18 +0800

    server_test

--- test_mlp/mlp_change_glass0_train&test/train_correct.py
+++ test_mlp/mlp_change_glass0_train&test/train_correct.py
@@ -218,8 +218,8 @@ init.normal_(net.output.weight, mean=0, std=0.01)
 init.constant_(net.hidden_1.bias, val=0)
 init.constant_(net.output.bias, val=0)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-if torch.cuda.device_count() > 1:
-    net = nn.DataParallel(net) # 包装为并行风格模型
+# if torch.cuda.device_count() > 1:
+#     net = nn.DataParallel(net) # 包装为并行风格模型
 net.to(device)
 
 loss = nn.BCELoss()  
