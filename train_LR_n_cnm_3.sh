d579cd1d2be2bb511ccff9697dbb7b817ea7f97e
taihan <taihan@bytedance.com>
2022-05-11 19:36:45 +0800

    train ncnm

---
+++ train_LR_n_cnm_3.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e
+
+
+mkdir -p ./test_yeast6/model_LR_concat_not_mirror/record_1/
+
+python3 ./classifier_LR/train_LR_concat_not_mirror.py dataset_name=yeast6 dataset_index=1 record_index=1
+python3 ./classifier_LR/train_LR_concat_not_mirror.py dataset_name=yeast6 dataset_index=2 record_index=1
+python3 ./classifier_LR/train_LR_concat_not_mirror.py dataset_name=yeast6 dataset_index=3 record_index=1
+python3 ./classifier_LR/train_LR_concat_not_mirror.py dataset_name=yeast6 dataset_index=4 record_index=1
+python3 ./classifier_LR/train_LR_concat_not_mirror.py dataset_name=yeast6 dataset_index=5 record_index=1
+
+
+
+mkdir -p ./test_yeast6/model_LR_normal/record_1/
+
+python3 ./classifier_LR/train_LR_normal.py dataset_name=yeast6 dataset_index=1 record_index=1
+python3 ./classifier_LR/train_LR_normal.py dataset_name=yeast6 dataset_index=2 record_index=1
+python3 ./classifier_LR/train_LR_normal.py dataset_name=yeast6 dataset_index=3 record_index=1
+python3 ./classifier_LR/train_LR_normal.py dataset_name=yeast6 dataset_index=4 record_index=1
+python3 ./classifier_LR/train_LR_normal.py dataset_name=yeast6 dataset_index=5 record_index=1
+
+
+
+mkdir -p ./test_ecoli1/model_LR_concat_not_mirror/record_1/
+
+python3 ./classifier_LR/train_LR_concat_not_mirror.py dataset_name=ecoli1 dataset_index=1 record_index=1
+python3 ./classifier_LR/train_LR_concat_not_mirror.py dataset_name=ecoli1 dataset_index=2 record_index=1
+python3 ./classifier_LR/train_LR_concat_not_mirror.py dataset_name=ecoli1 dataset_index=3 record_index=1
+python3 ./classifier_LR/train_LR_concat_not_mirror.py dataset_name=ecoli1 dataset_index=4 record_index=1
+python3 ./classifier_LR/train_LR_concat_not_mirror.py dataset_name=ecoli1 dataset_index=5 record_index=1
+
+
+
+mkdir -p ./test_ecoli1/model_LR_normal/record_1/
+
+python3 ./classifier_LR/train_LR_normal.py dataset_name=ecoli1 dataset_index=1 record_index=1
+python3 ./classifier_LR/train_LR_normal.py dataset_name=ecoli1 dataset_index=2 record_index=1
+python3 ./classifier_LR/train_LR_normal.py dataset_name=ecoli1 dataset_index=3 record_index=1
+python3 ./classifier_LR/train_LR_normal.py dataset_name=ecoli1 dataset_index=4 record_index=1
+python3 ./classifier_LR/train_LR_normal.py dataset_name=ecoli1 dataset_index=5 record_index=1
+
+
+
