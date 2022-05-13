f7edf89d0d6e8d56052807a740859fc6caea7d93
taihan <taihan@bytedance.com>
2022-05-14 03:00:16 +0800

    page test

--- generate_sh_file.py
+++ generate_sh_file.py
@@ -19,6 +19,82 @@
 # 根据不同的 dataset 大小 划分不同的组，生成执行脚本
 
 
+import sys
+
+# train method
+model_type_list = ['LR', 'SVMRBF', 'SVMPOLY']
+trans_method_list = ['minus', 'concat']
+mirror_method_list = ['Mirror', 'notMirror']
+
+
+ref_data_type_list = ['random', 'pos', 'neg']
+ref_num_type_list = ['num', 'IR']
+ref_times_dict = {
+    'num' : ['10', '20', '30', '40'],
+    'IR' : ['1', '2', '3', '4']
+}
+boundary_type_list = ['half', '1', '3']
+
+
+trans_test_method_list = [  ]
+
+data_range = 5
+record_index = 1
+bash_file_name = 'test_pageblocks1.sh'
+
+dataset = 'pageblocks1'
+
+
+
+with open(bash_file_name,'w') as fsh:
+    fsh.write('#!/bin/bash\n')
+    fsh.write('set -e\n\n\n')
+
+    # 生成训练方法
+    for model_type in model_type_list:
+        for trans_method in trans_method_list:
+            for mirror_method in mirror_method_list:
+                train_method = '{0}_{1}_{2}'.format(model_type, trans_method, mirror_method)
+
+                for ref_data_type in ref_data_type_list:
+                    for ref_num_type in ref_num_type_list:
+                        cur_time_list = ref_times_dict[ref_num_type]
+                        for ref_times in cur_time_list:
+                            for boundary_type in boundary_type_list:
+                                test_method = '{0}_{1}_{2}_{3}_{4}'.format(trans_method, ref_data_type, ref_num_type, ref_times, boundary_type)
+
+                                fsh.write('mkdir -p ./test_{0}/result_{1}_{2}/record_{3}/\n\n'.format(dataset, train_method, test_method, record_index))
+                                for dataset_index in range(1, 1+data_range):
+                                    fsh.write('python3 ./classifier_LR_SVM/test.py dataset_name={0} dataset_index={1} record_index=1 train_method={2} test_method={3}\n'.format(dataset, dataset_index, train_method, test_method))
+                                fsh.write('\n\n\n')
+        
+        train_method = 'LR_normal'
+        test_method = 'normal'
+        fsh.write('mkdir -p ./test_{0}/result_{1}_{2}/record_{3}/\n\n'.format(dataset, train_method, test_method, record_index))
+        for dataset_index in range(1, 1+data_range):
+            fsh.write('python3 ./classifier_LR_SVM/test.py dataset_name={0} dataset_index={1} record_index=1 train_method={2} test_method={3}\n'.format(dataset, dataset_index, train_method, test_method))
+        fsh.write('\n\n\n')
+
+        train_method = 'SVMRBF'
+        test_method = 'normal'
+        fsh.write('mkdir -p ./test_{0}/result_{1}_{2}/record_{3}/\n\n'.format(dataset, train_method, test_method, record_index))
+        for dataset_index in range(1, 1+data_range):
+            fsh.write('python3 ./classifier_LR_SVM/test.py dataset_name={0} dataset_index={1} record_index=1 train_method={2} test_method={3}\n'.format(dataset, dataset_index, train_method, test_method))
+        fsh.write('\n\n\n')
+
+        train_method = 'SVMPOLY'
+        test_method = 'normal'
+        fsh.write('mkdir -p ./test_{0}/result_{1}_{2}/record_{3}/\n\n'.format(dataset, train_method, test_method, record_index))
+        for dataset_index in range(1, 1+data_range):
+            fsh.write('python3 ./classifier_LR_SVM/test.py dataset_name={0} dataset_index={1} record_index=1 train_method={2} test_method={3}\n'.format(dataset, dataset_index, train_method, test_method))
+        fsh.write('\n\n\n')
+
+
+
+# ------------------------------- 任务 ----------------------------------------
+# 根据不同的 dataset 大小 划分不同的组，生成执行脚本
+
+
 # import sys
 
 # dataset_dict = {
@@ -106,59 +182,59 @@
 # 根据不同的 dataset 大小 划分不同的组，生成执行脚本
 
 
-import sys
+# import sys
 
-# SVM_dataset_list = ['abalone19', 'ecoli1', 'glass0', 'glass5', 'pima', 'vehicle0', 'yeast3', 'yeast5', 'yeast6']
-dataset_list = ['pageblocks1']
-# LR_dataset_list = ['abalone19', 'ecoli1', 'glass0', 'glass5', 'pageblocks1', 'pima', 'vehicle0', 'yeast3', 'yeast5', 'yeast6']
-# train_method = 'LR_concat_Mirror'
-# old_train_method = 'LR_concat_mirror'
+# # SVM_dataset_list = ['abalone19', 'ecoli1', 'glass0', 'glass5', 'pima', 'vehicle0', 'yeast3', 'yeast5', 'yeast6']
+# dataset_list = ['pageblocks1']
+# # LR_dataset_list = ['abalone19', 'ecoli1', 'glass0', 'glass5', 'pageblocks1', 'pima', 'vehicle0', 'yeast3', 'yeast5', 'yeast6']
+# # train_method = 'LR_concat_Mirror'
+# # old_train_method = 'LR_concat_mirror'
 # new_train_method = 'LR_concat_Mirror_new'
 
 
-method_map_list = [
-    ('SVM_POLY','SVMPOLY'),
-    ('SVM_POLY_concat_mirror','SVMPOLY_concat_Mirror'),
-    ('SVM_POLY_concat_not_mirror','SVMPOLY_concat_notMirror'),
-    ('SVM_POLY_minus_mirror','SVMPOLY_minus_Mirror'),
-    ('SVM_POLY_minus_not_mirror','SVMPOLY_minus_notMirror'),
-    ('SVM_RBF','SVMRBF'),
-    ('SVM_RBF_concat_mirror','SVMRBF_concat_Mirror'),
-    ('SVM_RBF_concat_not_mirror','SVMRBF_concat_notMirror'),
-    ('SVM_RBF_minus_mirror','SVMRBF_minus_Mirror'),
-    ('SVM_RBF_minus_not_mirror','SVMRBF_minus_notMirror')
-]
+# method_map_list = [
+#     ('SVM_POLY','SVMPOLY'),
+#     ('SVM_POLY_concat_mirror','SVMPOLY_concat_Mirror'),
+#     ('SVM_POLY_concat_not_mirror','SVMPOLY_concat_notMirror'),
+#     ('SVM_POLY_minus_mirror','SVMPOLY_minus_Mirror'),
+#     ('SVM_POLY_minus_not_mirror','SVMPOLY_minus_notMirror'),
+#     ('SVM_RBF','SVMRBF'),
+#     ('SVM_RBF_concat_mirror','SVMRBF_concat_Mirror'),
+#     ('SVM_RBF_concat_not_mirror','SVMRBF_concat_notMirror'),
+#     ('SVM_RBF_minus_mirror','SVMRBF_minus_Mirror'),
+#     ('SVM_RBF_minus_not_mirror','SVMRBF_minus_notMirror')
+# ]
 
 
-data_range = 5
-record_index = 1
-bash_file_name = 'change_name.sh'
+# data_range = 5
+# record_index = 1
+# bash_file_name = 'change_name.sh'
 
-with open(bash_file_name,'w') as fsh:
-    fsh.write('#!/bin/bash\n')
-    fsh.write('set -e\n\n\n')
+# with open(bash_file_name,'w') as fsh:
+#     fsh.write('#!/bin/bash\n')
+#     fsh.write('set -e\n\n\n')
 
-    # for dataset in dataset_list:
-    dataset = 'pageblocks1'
-    fsh.write('cd ./test_{0}/\n'.format(dataset))
-    for old_train_method, new_train_method in method_map_list:
-        fsh.write('mv model_{0} model_{1}\n'.format(old_train_method, new_train_method))
-        fsh.write('cd model_{0}/record_{1}\n\n'.format(new_train_method, record_index))
+#     # for dataset in dataset_list:
+#     dataset = 'pageblocks1'
+#     fsh.write('cd ./test_{0}/\n'.format(dataset))
+#     for old_train_method, new_train_method in method_map_list:
+#         fsh.write('mv model_{0} model_{1}\n'.format(old_train_method, new_train_method))
+#         fsh.write('cd model_{0}/record_{1}\n\n'.format(new_train_method, record_index))
 
-        for dataset_index in range(1, 1+data_range):
-            fsh.write('mv {0}_{1}.m {2}_{1}.m\n'.format(old_train_method, dataset_index, new_train_method))
-        fsh.write('cd ../../\n')
+#         for dataset_index in range(1, 1+data_range):
+#             fsh.write('mv {0}_{1}.m {2}_{1}.m\n'.format(old_train_method, dataset_index, new_train_method))
+#         fsh.write('cd ../../\n')
         
-        fsh.write('\n\n')
+#         fsh.write('\n\n')
 
-    # for dataset in SVM_dataset_list:
-    #     fsh.write('cd ./test_{0}/model_MLP_normal/record_{1}\n'.format(dataset, record_index))
+#     # for dataset in SVM_dataset_list:
+#     #     fsh.write('cd ./test_{0}/model_MLP_normal/record_{1}\n'.format(dataset, record_index))
 
 
-    #     for dataset_index in range(1, 1+data_range):
-    #         fsh.write('mv normal_MLP_{1} MLP_normal_{1}\n'.format(old_train_method, dataset_index, new_train_method))
-    #     fsh.write('cd ../../../\n')
-    #     fsh.write('\n\n\n')
+#     #     for dataset_index in range(1, 1+data_range):
+#     #         fsh.write('mv normal_MLP_{1} MLP_normal_{1}\n'.format(old_train_method, dataset_index, new_train_method))
+#     #     fsh.write('cd ../../../\n')
+#     #     fsh.write('\n\n\n')
 
 
 
