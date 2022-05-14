b63e9908fc2af6b315f811ada635d8734df08870
taihan <taihan@bytedance.com>
2022-05-14 14:32:04 +0800

    split test

--- test_LR_SVM_4.sh
+++ test_LR_SVM_4.sh
@@ -3,76 +3,6 @@ set -e
 
 
 
-mkdir -p ./test_abalone19/result_SVMRBF_concat_notMirror_concat_neg_IR_3_3/record_1/
-
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=1 record_index=1 train_method=SVMRBF_concat_notMirror test_method=concat_neg_IR_3_3
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=2 record_index=1 train_method=SVMRBF_concat_notMirror test_method=concat_neg_IR_3_3
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=3 record_index=1 train_method=SVMRBF_concat_notMirror test_method=concat_neg_IR_3_3
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=4 record_index=1 train_method=SVMRBF_concat_notMirror test_method=concat_neg_IR_3_3
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=5 record_index=1 train_method=SVMRBF_concat_notMirror test_method=concat_neg_IR_3_3
-
-
-
-mkdir -p ./test_abalone19/result_SVMRBF_concat_notMirror_concat_neg_IR_4_half/record_1/
-
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=1 record_index=1 train_method=SVMRBF_concat_notMirror test_method=concat_neg_IR_4_half
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=2 record_index=1 train_method=SVMRBF_concat_notMirror test_method=concat_neg_IR_4_half
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=3 record_index=1 train_method=SVMRBF_concat_notMirror test_method=concat_neg_IR_4_half
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=4 record_index=1 train_method=SVMRBF_concat_notMirror test_method=concat_neg_IR_4_half
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=5 record_index=1 train_method=SVMRBF_concat_notMirror test_method=concat_neg_IR_4_half
-
-
-
-mkdir -p ./test_abalone19/result_SVMRBF_concat_notMirror_concat_neg_IR_4_1/record_1/
-
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=1 record_index=1 train_method=SVMRBF_concat_notMirror test_method=concat_neg_IR_4_1
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=2 record_index=1 train_method=SVMRBF_concat_notMirror test_method=concat_neg_IR_4_1
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=3 record_index=1 train_method=SVMRBF_concat_notMirror test_method=concat_neg_IR_4_1
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=4 record_index=1 train_method=SVMRBF_concat_notMirror test_method=concat_neg_IR_4_1
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=5 record_index=1 train_method=SVMRBF_concat_notMirror test_method=concat_neg_IR_4_1
-
-
-
-mkdir -p ./test_abalone19/result_SVMRBF_concat_notMirror_concat_neg_IR_4_3/record_1/
-
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=1 record_index=1 train_method=SVMRBF_concat_notMirror test_method=concat_neg_IR_4_3
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=2 record_index=1 train_method=SVMRBF_concat_notMirror test_method=concat_neg_IR_4_3
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=3 record_index=1 train_method=SVMRBF_concat_notMirror test_method=concat_neg_IR_4_3
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=4 record_index=1 train_method=SVMRBF_concat_notMirror test_method=concat_neg_IR_4_3
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=5 record_index=1 train_method=SVMRBF_concat_notMirror test_method=concat_neg_IR_4_3
-
-
-
-mkdir -p ./test_abalone19/result_LR_normal_normal/record_1/
-
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=1 record_index=1 train_method=LR_normal test_method=normal
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=2 record_index=1 train_method=LR_normal test_method=normal
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=3 record_index=1 train_method=LR_normal test_method=normal
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=4 record_index=1 train_method=LR_normal test_method=normal
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=5 record_index=1 train_method=LR_normal test_method=normal
-
-
-
-mkdir -p ./test_abalone19/result_SVMRBF_normal/record_1/
-
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=1 record_index=1 train_method=SVMRBF test_method=normal
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=2 record_index=1 train_method=SVMRBF test_method=normal
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=3 record_index=1 train_method=SVMRBF test_method=normal
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=4 record_index=1 train_method=SVMRBF test_method=normal
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=5 record_index=1 train_method=SVMRBF test_method=normal
-
-
-
-mkdir -p ./test_abalone19/result_SVMPOLY_normal/record_1/
-
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=1 record_index=1 train_method=SVMPOLY test_method=normal
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=2 record_index=1 train_method=SVMPOLY test_method=normal
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=3 record_index=1 train_method=SVMPOLY test_method=normal
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=4 record_index=1 train_method=SVMPOLY test_method=normal
-python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=5 record_index=1 train_method=SVMPOLY test_method=normal
-
-
-
 mkdir -p ./test_abalone19/result_SVMPOLY_minus_Mirror_minus_random_num_10_half/record_1/
 
 python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=1 record_index=1 train_method=SVMPOLY_minus_Mirror test_method=minus_random_num_10_half
@@ -970,4 +900,26 @@ python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=2 recor
 python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=3 record_index=1 train_method=SVMPOLY_minus_notMirror test_method=minus_random_IR_2_3
 python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=4 record_index=1 train_method=SVMPOLY_minus_notMirror test_method=minus_random_IR_2_3
 python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=5 record_index=1 train_method=SVMPOLY_minus_notMirror test_method=minus_random_IR_2_3
+
+
+
+
+
+mkdir -p ./test_abalone19/result_SVMRBF_normal/record_1/
+
+python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=1 record_index=1 train_method=SVMRBF test_method=normal
+python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=2 record_index=1 train_method=SVMRBF test_method=normal
+python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=3 record_index=1 train_method=SVMRBF test_method=normal
+python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=4 record_index=1 train_method=SVMRBF test_method=normal
+python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=5 record_index=1 train_method=SVMRBF test_method=normal
+
+
+
+mkdir -p ./test_abalone19/result_SVMPOLY_normal/record_1/
+
+python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=1 record_index=1 train_method=SVMPOLY test_method=normal
+python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=2 record_index=1 train_method=SVMPOLY test_method=normal
+python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=3 record_index=1 train_method=SVMPOLY test_method=normal
+python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=4 record_index=1 train_method=SVMPOLY test_method=normal
+python3 ./classifier_LR_SVM/test.py dataset_name=abalone19 dataset_index=5 record_index=1 train_method=SVMPOLY test_method=normal
 
